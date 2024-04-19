599f417029b01324cb945ebc39118e55acd4b909
rogeriop <pinheiroxrogerio@gmail.com>
2024-04-19 15:52:36 -0300

    01 Iniciando com JavaScript

---
+++ desafios/app01.js
@@ -0,0 +1,35 @@
+
+
+// Mostre um alerta com a mensagem "Boas vindas ao nosso site!"
+alert('Boas vindas ao nosso site');
+
+// Declare uma variável chamada nome e atribua a ela o valor "Lua"
+let nome;
+nome = "Lua";
+
+// Crie uma variável chamada idade e atribua a ela o valor 25
+let idade = 25;
+
+// Defina uma variável numeroDeVendas e atribua a ela o valor 50
+let numeroDeVendas = 50;
+
+// Defina uma variável saldoDisponivel e atribua a ela o valor 1000
+let saldoDisponivel = 1000;
+
+// Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert('Erro! Preencha todos os campos');
+
+// Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+// Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome
+let nomeUsuario;
+nomeUsuario = prompt('Informe seu nome:');
+
+// Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade
+let idadeUsuario = prompt('Informe a sua idade:')
+// Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!"
+if (idadeUsuario >= 18) {
+    alert('Pode tirar a habilitação!');
+}
